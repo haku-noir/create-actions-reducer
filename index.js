58a54ca988baee07e83aca27acaa3cc74f186a6b
haku-noir <hakunoir.00@gmail.com>
2020-04-24 10:57:19 +0900

    change: index.jsを引数がある関数に変更し、programをmainへ移動

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-#!/usr/bin/env node
-'use strict'
-
-const program = require('commander');
 const fs = require('fs-extra');
 
 const getSrcDir = dir => {
@@ -18,19 +14,12 @@ const getSrcDir = dir => {
 const getHeadLower = str => str.slice(0, 1).toLowerCase() + str.slice(1);
 const getHeadUpper = str => str.slice(0, 1).toUpperCase() + str.slice(1);
 
-module.exports = () => {
-  program
-    .usage('<reducer_name> [options]')
-    .option('-m, --minimum', 'create minimum app')
-    .version(require(`${__dirname}/package.json`).version, '-v, --version')
-    .parse(process.argv);
-
+module.exports = (name, minimum) => {
   const srcDir = getSrcDir(process.cwd());
 
-  const name = program.args[0];
   if(!name){
     console.log('<reducer_name> is required\n');
-    console.log(program.help());
+    console.log(help());
     process.exit();
   }
   const xxx = getHeadLower(name);
@@ -38,7 +27,7 @@ module.exports = () => {
 
   Promise.resolve()
     .then(() => (
-      program.minimum ?
+      minimum ?
         fs.readFile(`${__dirname}/minimum/minimumActions.ts`, 'utf8') :
         fs.readFile(`${__dirname}/sample/sampleActions.ts`, 'utf8')
     ))
@@ -47,7 +36,7 @@ module.exports = () => {
       return fs.outputFile(`${srcDir}/actions/${xxx}Actions.ts`, replacedData);
     })
     .then(() => (
-      program.minimum ?
+      minimum ?
         fs.readFile(`${__dirname}/minimum/minimumReducer.ts`, 'utf8') :
         fs.readFile(`${__dirname}/sample/sampleReducer.ts`, 'utf8')
     ))
